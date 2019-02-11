4fa2097c5a789b0e6022d8381f9932dd807c1382
Bruno Luiz Silva <contact@brunoluiz.net>
2019-02-11 19:00:24 +0000

    feat: improve typography

--- src/utils/typography.js
+++ src/utils/typography.js
@@ -1,17 +1,11 @@
-import Typography from "typography"
-import Wordpress2016 from "typography-theme-wordpress-2016"
+import Typography from 'typography'
+import CodePlugin from 'typography-plugin-code'
+import theme from 'typography-theme-moraga'
 
-Wordpress2016.overrideThemeStyles = () => {
-  return {
-    "a.gatsby-resp-image-link": {
-      boxShadow: `none`,
-    },
-  }
-}
+theme.plugins = [new CodePlugin()]
 
-delete Wordpress2016.googleFonts
-
-const typography = new Typography(Wordpress2016)
+const typography = new Typography(theme)
+// delete theme.googleFonts
 
 // Hot reload typography in development.
 if (process.env.NODE_ENV !== `production`) {
